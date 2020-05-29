19d9416dbb419d83885fb92d7cc58229abeb4a1e
coinicles <coinicles@gmail.com>
2020-05-29 11:29:04 +0100

    added chinese

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -7,7 +7,9 @@ const languages = [
   { name: "German", code: "de", flag: "de" },
   { name: "French", code: "fr", flag: "fr" },
   { name: "Spanish", code: "es", flag: "es" },
-  { name: "Portuguese", code: "pt-br", flag: "pt" }
+  { name: "Portuguese", code: "pt-br", flag: "pt" },
+  { name: "China", code: "zh_cn", flag: "cn" }
+
 ];
 
 export { languages };
